a9e7963e80e534138d4cf197a22d1eab9f0b5c72
vitkon0705 <vitkon0705@gmail.com>
2025-10-20 22:17:35 +0300

    Button -> LED. Warning about short cirquit SimulIDE doesn't show!

--- main.c
+++ main.c
@@ -1,17 +1,19 @@
-#define F_CPU 16000000UL // frequency CPU is 16MHz. By default it is 1MHz This string must be before #include, UL - unsigned long type. It must be equal to properties in SimulIDE
-
 #include <avr/io.h> // Ubuntu/usr/lib/avr/include/avr/io.h
-#include <util/delay.h>
 
-int main(void) {
+int main(void)
+{
  // Set port D bits: 0 - input, 1 - output
- DDRD = 129; // 0b1000`0001
+ DDRC = 64;
+ // DDRB = 1; // Short cirquit if button is pushed!
+ PORTB = 1; // by default DDRx = 0 and PORTx = 0. Set PORTB = 1 for reading if the button (connected with earth) is pushed
  
- while(1) {
-  // Set port D bits signal level: 0 - +0V, 1 - +5V
-  PORTD = 1;
-  _delay_ms(10000); // wait 10 sec It will be real 10`000 millisec if only #define F_CPU (Hz) is equal to properties in SimulIDE (MHz)
-  PORTD = 128;
-  _delay_ms(10000);
+ while(1) // endless cycle
+ {
+  while (PINB == 0) // Button is pushed (short cirqut if DDRB = 1)
+  {
+   PORTC = 64; // Switch on the LED
+  } 
+  // Button isn't pushed
+  PORTC = 0; // Switch off the LED
  }
 }
